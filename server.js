f91b310d2ca70c866d02d6f612cb3b77f0c593b3
Ben Huang <yfenghuang@gmail.com>
2019-03-03 13:43:49 -0800

    fixes url history fallback

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const history = require('connect-history-api-fallback');
+//let logger = require('morgan');
+
+//const bodyParser = require('body-parser');
+
+const app = express();
+
+//app.use(logger('dev'));
+//app.use(bodyParser.json());
+
+const staticFileMiddleware = express.static('dist');
+app.use(staticFileMiddleware);
+app.use(history());
+app.use(staticFileMiddleware);
+
+// Serve the index.html for all the other requests so that the
+// router in the javascript app can render the necessary components
+/*app.get('*',function(req,res){
+    res.sendFile(__dirname + '/dist/index.html');
+});*/
+
+// If no route is matched by now, it must be a 404
+app.use(function(req, res, next) {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// Start the server
+app.set('port', process.env.PORT || 8081);
+
+let server = app.listen(app.get('port'), () => {
+    console.log('Express server listening on port ' + server.address().port);
+});
