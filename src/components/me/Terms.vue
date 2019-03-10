d372e5ff6bb72060d333cccae8e33d52b928d273
Ben Huang <yfenghuang@gmail.com>
2019-03-10 16:19:01 -0700

    adds missing slide animation style

--- src/components/me/Terms.vue
+++ src/components/me/Terms.vue
@@ -264,4 +264,10 @@
         margin-top: 30px;
         font-size: $ft-form-normal;
     }
+    .slide-enter-active,.slide-leave-active{
+        transition: all 0.3s;
+    }
+    .slide-enter,.slide-leave-to{
+        transform: translate3d(100%, 0, 0);
+    }
 </style>
